f1fcb8580ad6dcb9cb07a2f11653fd4dbe6864dc
bassam <alsaffar.bassam@gmail.com>
2025-02-12 12:32:20 +0100

    added Components with Header and Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", 
     "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5f6fff"
+      }
+    },
   },
   plugins: [],
 };
