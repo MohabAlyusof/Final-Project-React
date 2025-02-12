7422fdefb4f6d21d29f591e8cba353bde717f3fc
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-02-12 10:58:36 +0100

    Add a folder Pages with 8 file and change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import MyProfile from "./Pages/MyProfile";
+import MyAppointments from "./Pages/MyAppointments";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
