7ac85d2d41e1ff8c7a9672da11a86c9ef2fc139d
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-02-13 19:02:12 +0100

    new change in index.css and Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,8 +4,10 @@ import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+
   const [showMenu, setShowMenu] = useState(false);
   const [token, setToken] = useState(true);
+
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
       <img
@@ -15,7 +17,7 @@ const Navbar = () => {
         alt=""
       />
       <ul className="md:flex items-start gap-5 font-medium hidden">
-        <NavLink to="/home">
+        <NavLink to="/">
           <li className="py-1">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
@@ -32,6 +34,7 @@ const Navbar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
+
       <div className="flex items-center gap-4 ">
         {token ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
@@ -77,8 +80,38 @@ const Navbar = () => {
           src={assets.menu_icon}
           alt=""
         />
+        <div
+          className={`md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all ${
+            showMenu ? "fixed w-full" : "h-0 w-0"
+          }`}
+        >
+          <div className="flex items-center justify-between px-5 py-6">
+            <img src={assets.logo} className="w-36" alt="" />
+            <img
+              onClick={() => setShowMenu(false)}
+              src={assets.cross_icon}
+              className="w-7"
+              alt=""
+            />
+          </div>
+          <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+            <NavLink onClick={() => setShowMenu(false)} to="/">
+              <p className="px-4 py-2 rounded full inline-block">HOME</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/doctors">
+              <p className="px-4 py-2 rounded full inline-block">ALL DOCTORS</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/about">
+              <p className="px-4 py-2 rounded full inline-block">ABOUT</p>
+            </NavLink>
+            <NavLink onClick={() => setShowMenu(false)} to="/contact">
+              <p className="px-4 py-2 rounded full inline-block">CONTACT</p>
+            </NavLink>
+          </ul>
+        </div>
       </div>
     </div>
   );
 };
+
 export default Navbar;
