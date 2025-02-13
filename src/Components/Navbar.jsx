04ed14a0d9bba825b46bc7c43f0435c6ce2e1298
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-02-13 16:38:59 +0100

    check

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,15 +4,18 @@ import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
-
   const [showMenu, setShowMenu] = useState(false);
   const [token, setToken] = useState(true);
-  
   return (
-    <div className="flex justify-between items-center text-sm py-4 mb-5 border-b-gray-400">
-      <img onClick={()=>navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="assets" />
-      <ul className="hidden md:flex items-start gap-5 font-medium">
-        <NavLink to="/">
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img
+        onClick={() => navigate("/")}
+        className="w-44 cursor-pointer"
+        src={assets.logo}
+        alt=""
+      />
+      <ul className="md:flex items-start gap-5 font-medium hidden">
+        <NavLink to="/home">
           <li className="py-1">HOME</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
@@ -29,7 +32,7 @@ const Navbar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 ">
         {token ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
             <img className="w-8 rounded-full" src={assets.profile_pic} alt="" />
@@ -37,19 +40,22 @@ const Navbar = () => {
             <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
-                  onClick={() => navigate("my-profile")}
+                  onClick={() => navigate("/my-profile")}
                   className="hover:text-black cursor-pointer"
                 >
                   My Profile
                 </p>
                 <p
-                  onClick={() => navigate("my-appointments")}
+                  onClick={() => navigate("/my-appointments")}
                   className="hover:text-black cursor-pointer"
                 >
                   My Appointments
                 </p>
                 <p
-                  onClick={() => setToken(false)}
+                  onClick={() => {
+                    setToken(false);
+                    navigate("/");
+                  }}
                   className="hover:text-black cursor-pointer"
                 >
                   Logout
@@ -57,8 +63,7 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-        ) 
-        : (
+        ) : (
           <button
             onClick={() => navigate("/login")}
             className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block"
@@ -66,9 +71,14 @@ const Navbar = () => {
             Create account
           </button>
         )}
+        <img
+          onClick={() => setShowMenu(true)}
+          className="w-6 md:hidden"
+          src={assets.menu_icon}
+          alt=""
+        />
       </div>
     </div>
   );
 };
-
 export default Navbar;
