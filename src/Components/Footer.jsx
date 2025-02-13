b925adc7972f5c8a558b6d7f0b6514204dd08038
bassam <alsaffar.bassam@gmail.com>
2025-02-13 12:33:39 +0100

    add About and Contact link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -14,10 +15,14 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+          <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About us</Link>
+        </li>
+
           </ul>
         </div>
 
@@ -25,7 +30,7 @@ const Footer = () => {
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
             <li>+49157591055552</li>
-            <li>#@gmail.com</li>
+            <li>Prescripto@Prescripto.com</li>
           </ul>
         </div>
 
