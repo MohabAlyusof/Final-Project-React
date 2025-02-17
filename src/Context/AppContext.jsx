12e5b8c96531802968ea94d124bce86db58cd1cf
Mohab Alyusof <Mohabalusef-1997@hotmail.com>
2025-02-17 16:24:25 +0100

    new Change

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,8 +1,6 @@
 import { createContext } from 'react';
 import { doctors } from '../assets/assets';
-
 export const AppContext = createContext();
-
 const AppContextProvider = (props) => {
     function login(email, password) {
         const storedUser = JSON.parse(localStorage.getItem("currentUser"));
@@ -15,27 +13,21 @@ const AppContextProvider = (props) => {
             return { success: false, message: "Invalid email or password." };
         }
     }
-
     function register(name, email, password) {
         const storedUser = JSON.parse(localStorage.getItem("currentUser"));
-        if (storedUser.email === email && storedUser.password === password
-        ) {
+        if (storedUser && storedUser.email === email && storedUser.password === password) {
             return { success: false, message: "A user is already registered. Please log in." };
         }
         localStorage.setItem("currentUser", JSON.stringify({ name, email, password }));
         return { success: true, message: "Registration successful!" };
     }
-
     const value = {
         doctors, login, register
     }
-
     return (
         <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
     )
 }
-
 export default AppContextProvider;
-
